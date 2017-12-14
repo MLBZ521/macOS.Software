a717bb165ed8d9c174f097c131e82c43abd3c513
Zack T <MLBZ521@gmail.com>
2017-12-14 14:45:36 -0700

    Added binary path, minor changes to verbiage
     + Added binary path
     + Minor changes to verbiage

--- Scripts/install_ParallelsLicense.sh
+++ Scripts/install_ParallelsLicense.sh
@@ -3,16 +3,24 @@
 ###################################################################################################
 # Script Name:  install_ParallelsLicense.sh
 # By:  Zack Thompson / Created:  8/17/2017
-# Version:  1.0 / Updated:  8/17/2017 / By:  ZT
+# Version:  1.0 / Updated:  12/14/2017 / By:  ZT
 #
-# Description:  This script will allow the installation of the Parallels License.
+# Description:  This script will apply a Parallels License provided as a JSS Script Parameter.
 #
 ###################################################################################################
 
-/bin/echo "Installing the Parallels License..."
+/bin/echo "******************************************************"
+/bin/echo "*****  install_ParallelsLicense Process:  START  *****"
+/bin/echo "******************************************************"
 
-prlsrvctl install-license -k $4
+/bin/echo "Applying the Parallels license..."
 
-/bin/echo "License installed!"
+/usr/local/bin/prlsrvctl install-license -k $4
+
+/bin/echo "Parallels has been licensed!"
+
+/bin/echo "*****************************************************"
+/bin/echo "*****  install_ParallelsLicense Process:  STOP  *****"
+/bin/echo "*****************************************************"
 
 exit 0
