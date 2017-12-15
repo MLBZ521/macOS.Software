7a832d99837485b5e8686e988fbd4ae9972c2551
Zack T <MLBZ521@gmail.com>
2017-12-14 17:04:13 -0700

    v1.1 = Added verfication checks
     + Added verfication checks

--- Scripts/install_ParallelsLicense.sh
+++ Scripts/install_ParallelsLicense.sh
@@ -3,24 +3,33 @@
 ###################################################################################################
 # Script Name:  install_ParallelsLicense.sh
 # By:  Zack Thompson / Created:  8/17/2017
-# Version:  1.0 / Updated:  12/14/2017 / By:  ZT
+# Version:  1.1 / Updated:  12/14/2017 / By:  ZT
 #
 # Description:  This script will apply a Parallels License provided as a JSS Script Parameter.
 #
 ###################################################################################################
 
-/bin/echo "******************************************************"
 /bin/echo "*****  install_ParallelsLicense Process:  START  *****"
-/bin/echo "******************************************************"
 
-/bin/echo "Applying the Parallels license..."
+# Define Variables
+Parallels="/usr/local/bin/prlsrvctl"
 
-/usr/local/bin/prlsrvctl install-license -k $4
+if [[ ! -x $Parallels ]]; then
+    /bin/echo "Error:  Parallels is not properly installed."
+	/bin/echo "*****  install_ParallelsLicense Process:  FAILED  *****"
+    exit 1
+else
+	/bin/echo "Applying the Parallels license..."
+	$Parallels install-license -k $4
+	exitCode=$?
 
-/bin/echo "Parallels has been licensed!"
-
-/bin/echo "*****************************************************"
-/bin/echo "*****  install_ParallelsLicense Process:  STOP  *****"
-/bin/echo "*****************************************************"
+	if [[ $exitCode = 0 ]]; then
+		/bin/echo "Parallels has been licensed!"
+		/bin/echo "*****  install_ParallelsLicense Process:  COMPLETE  *****"
+	else
+		/bin/echo "Error:  License was likely invalid."
+		/bin/echo "*****  install_ParallelsLicense Process:  FAILED  *****"
+		exit 2
+fi
 
 exit 0
