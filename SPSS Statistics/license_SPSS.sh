c723e560b205ca148a518dabe2714322a18ee8f1
Zack T <MLBZ521@gmail.com>
2019-01-12 21:24:43 -0700

    v1.6.0 = Updated to resolve several permission-related issues
    + Updated to resolve several permission-related issues described here:  https://www-01.ibm.com/support/docview.wss?uid=swg21966637
    + Code formatting changes

--- SPSS Statistics/license_SPSS.sh
+++ SPSS Statistics/license_SPSS.sh
@@ -3,7 +3,7 @@
 ###################################################################################################
 # Script Name:  license_SPSS.sh
 # By:  Zack Thompson / Created:  1/3/2018
-# Version:  1.5.1 / Updated:  4/2/2018 / By:  ZT
+# Version:  1.6.0 / Updated:  1/12/2019 / By:  ZT
 #
 # Description:  This script applies the license for SPSS applications.
 #
@@ -19,15 +19,15 @@ shopt -s nocasematch
 case "${4}" in
 	"T&R" | "Teaching and Research" | "Academic" )
 		licenseType="Academic"
-		;;
+	;;
 	"Admin" | "Administrative" )
 		licenseType="Administrative"
-		;;
+	;;
 	* )
 		echo "ERROR:  Invalid License Type provided"
 		echo "*****  License SPSS process:  FAILED  *****"
 		exit 1
-		;;
+	;;
 esac
 
 echo "Licensing Type:  ${licenseType}"
@@ -36,15 +36,15 @@ echo "Licensing Type:  ${licenseType}"
 case "${5}" in
 	"LM" | "License Manager" | "Network" )
 		licenseMechanism="Network"
-		;;
+	;;
 	"Stand Alone" | "Local" )
 		licenseMechanism="Local"
-		;;
+	;;
 	* )
 		echo "ERROR:  Invalid License Mechanism provided"
 		echo "*****  License SPSS process:  FAILED  *****"
 		exit 2
-		;;
+	;;
 esac
 
 # Turn off case-insensitive pattern matching
@@ -64,13 +64,13 @@ function LicenseInfo {
 		case "${versionSPSS}" in
 			"25" )
 				licenseCode="2512345678910"
-				;;
+			;;
 			"24" )
 				licenseCode="2412345678910"
-				;;
+			;;
 			"23" )
 				licenseCode="2312345678910"
-				;;
+			;;
 		esac
 	elif [[ $licenseType == "Administrative" ]]; then
 		licenseManager="server.company.com"
@@ -80,13 +80,13 @@ function LicenseInfo {
 		case "${versionSPSS}" in
 			"25" )
 				licenseCode="2512345678911"
-				;;
+			;;
 			"24" )
 				licenseCode="2412345678911"
-				;;
+			;;
 			"23" )
 				licenseCode="2312345678911"
-				;;
+			;;
 		esac
 	fi
 }
@@ -95,9 +95,9 @@ function LicenseInfo {
 # Bits staged, license software...
 
 # Find all install SPSS versions.
-appPaths=$(/usr/bin/find -E /Applications -iregex ".*[/](SPSS) ?(Statistics) ?([0-9]{2})?[.]app" -type d -prune)
+appPaths=$( /usr/bin/find -E /Applications -iregex ".*[/](SPSS) ?(Statistics) ?([0-9]{2})?[.]app" -type d -prune )
 
-# Verify that a Maple version was found.
+# Verify that a SPSS version was found.
 if [[ -z "${appPaths}" ]]; then
 	echo "A version of SPSS was not found in the expected location!"
 	echo "*****  License SPSS process:  FAILED  *****"
@@ -106,16 +106,28 @@ else
 	# If the machine has multiple SPSS Applications, loop through them...
 	while IFS="\n" read -r appPath; do
 
+		##################################################
+		# Define Variables
+		# Get the SPSS version
+		versionSPSS=$( /usr/bin/defaults read "${appPath}/Contents/Info.plist" CFBundleShortVersionString | /usr/bin/awk -F "." '{print $1}' )
+		# Get the App Bundle name
+		appName=$( echo "${appPath}" | /usr/bin/awk -F "/" '{print $NF}' )
+		# Get only the install path
+		installPath=$( echo "${appPath}" | /usr/bin/awk -F "/${appName}" '{print $1}' )
 		# Set the location of bin folder
-			licensePath="${appPath}/Contents/bin"
+		licensePath="${appPath}/Contents/bin"
 		# Set the Network License file path
-			networkLicense="${licensePath}/spssprod.inf"
+		networkLicense="${licensePath}/spssprod.inf"
 		# Set the Local License Location file path
-			localLicense="${licensePath}/lservrc"
+		localLicense="${licensePath}/lservrc"
+		##################################################
+
+		# Setting permissions to resolve issues seen in:  https://www-01.ibm.com/support/docview.wss?uid=swg21966637
+		echo "Setting permissions on SPSS ${majorVersion} files..."
+		/usr/sbin/chown -R root:admin "${installPath}"
 
 		if [[ $licenseMechanism == "Network" ]]; then
-
-			echo "Configuring the License Manager Server..."
+			echo "Configuring the License Manager Server for version:  ${versionSPSS}"
 
 			# Function LicenseInfo
 			LicenseInfo
@@ -130,25 +142,26 @@ else
 			fi
 
 		elif [[ $licenseMechanism == "Local" ]]; then
-
-			# Get the SPSS version
-			versionSPSS=$(/usr/bin/defaults read "${appPath}/Contents/Info.plist" CFBundleShortVersionString | /usr/bin/awk -F "." '{print $1}')
 			echo "Apply License Code for version:  ${versionSPSS}"
 
 			# Function LicenseInfo
 			LicenseInfo
 
 			# Apply License Code
-			exitStatus=$(cd "${licensePath}" && "${licensePath}"/licenseactivator "${licenseCode}")
+			exitStatus=$( cd "${licensePath}" && "${licensePath}"/licenseactivator "${licenseCode}" )
 
 			if [[ $exitStatus == *"Authorization succeeded"* ]]; then
 				echo "License Code applied successfully!"
 
+				# If the network license file exists, remove the License Manager server name.
 				if [[ -e "${networkLicense}" ]]; then
 					echo "Removing Network License Manager info..."
-					# Remove the License Manager Server Name.
 					/usr/bin/sed -i '' 's/DaemonHost=.*/DaemonHost=/' "${networkLicense}"
 				fi
+
+				# Setting permissions to resolve issues as it relates to (this step is not described, but helps to resolve):  https://www-01.ibm.com/support/docview.wss?uid=swg21966637
+				echo "Setting permissions on the SPSS ${majorVersion} license file..."
+				/bin/chmod 644 "${localLicense}"
 
 			else
 				echo "ERROR:  Failed to apply License Code"
@@ -157,7 +170,7 @@ else
 				exit 4
 			fi
 		fi
-	done < <(echo "${appPaths}")
+	done < <( echo "${appPaths}" )
 fi
 
 echo "SPSS has been activated!"
