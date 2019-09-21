2549647d84f35a1b32601f726ddd81dca243e449
Zack T <MLBZ521@gmail.com>
2019-09-20 21:04:35 -0700

    v1.4.0 = Added the installation of the `Wolfram Script.pkg`
    + Added support to nest and install the Wolfram Script.pkg inside a payloadless package for deploying Mathematica

--- Mathematica/install_Mathematica.sh
+++ Mathematica/install_Mathematica.sh
@@ -3,7 +3,7 @@
 ###################################################################################################
 # Script Name:  install_Mathematica.sh
 # By:  Zack Thompson / Created:  1/10/2018
-# Version:  1.3 / Updated:  3/23/2018 / By:  ZT
+# Version:  1.4.0 / Updated:  9/20/2019 / By:  ZT
 #
 # Description:  This script silently installs Mathematica.
 #
@@ -15,7 +15,21 @@ echo "*****  Install Mathematica process:  START  *****"
 # Define Variables
 
 # Set working directory
-	pkgDir=$(/usr/bin/dirname "${0}")
+pkgDir=$(/usr/bin/dirname "${0}")
+
+##################################################
+# Define Functions
+
+exitCheck() {
+	if [[ $1 != 0 ]]; then
+		echo "Failed to install:  ${2}"
+		echo "Exit Code:  ${1}"
+		echo "*****  Install Mathematica process:  FAILED  *****"
+		exit 1
+	else
+		echo "${2} has been installed!"
+	fi
+}
 
 ##################################################
 # Bits staged...
@@ -26,10 +40,28 @@ if [[ -e "/Applications/Mathematica.app" ]]; then
 	/bin/rm -rf "/Applications/Mathematica.app"
 fi
 
+echo "Installing Mathematica.app..."
+
 # Install Mathematica
-echo "Installing Mathematica..."
-	/bin/mv "${pkgDir}/Mathematica.app" /Applications
-echo "Install complete!"
+/bin/mv "${pkgDir}/Mathematica.app" /Applications
+exitCode1=$?
+
+# Function exitCheck
+exitCheck $exitCode1 "Mathematica.app"
+
+# Get all of the pkgs.
+packages=$( /bin/ls "${pkgDir}/" | /usr/bin/grep .pkg )
+
+# Loop through each .pkg in the directory...
+while IFS=.pkg read pkg; do
+	echo "Installing ${pkg}..."
+	/usr/sbin/installer -dumplog -verbose -pkg "${pkgDir}/${pkg}" -allowUntrusted -target /
+	exitCode2=$?
+
+	# Function exitCheck
+	exitCheck $exitCode2 "${pkg}"
+
+done < <(echo "${packages}")
 
 echo "*****  Install Mathematica process:  COMPLETE  *****"
 
